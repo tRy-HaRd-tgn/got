c86fbfe4b9b9a596c3f78aff837b48a94932b81c
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-15 22:14:49 +0300

    feat: add api

---
+++ frontend/src/http/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const API_URL = "http://localhost:8000/";
+
+const $api = axios.create({
+  withCredentials: true,
+  baseURL: API_URL,
+});
+
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    if (error.response.status == 401) {
+      const originalRequest = error.config;
+      try {
+        const response = await AuthService.checkAuth();
+        localStorage.setItem("token", response.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.log("НЕ АВТОРИЗОВАН");
+      }
+    }
+  }
+);
+
+export default $api;
