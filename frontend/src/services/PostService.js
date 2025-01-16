b0d7ba5b02254930e608a0ec7e9d9e14a76ef7d1
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-16 20:53:15 +0300

    fix: fix service name feat: configure service

---
+++ frontend/src/services/PostService.js
@@ -0,0 +1,21 @@
+import $api from "../http";
+export default class PostsService {
+  static async getPost(id) {
+    // получение поста по id
+    return $api.post(`/api/posts/${id}`, { id });
+  }
+  static async updatePost(id) {
+    // обновление информации поста
+    return $api.put(`/api/posts/${id}`, { id });
+  }
+  static async getPosts() {
+    // получение всех постов
+    return $api.get("/api/posts");
+  }
+  static async createPost() {
+    return $api.post("/api/posts");
+  }
+  static async getPostImg(id) {
+    return $api.get(`/api/posts/${id}/profile`);
+  }
+}
--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,18 +1,18 @@
 import $api from "../http";
 export default class UserService {
-  static async login(email, password) {
-    return $api.post("/api/users/login", { email, password });
+  static async login(login, password) {
+    return $api.post("/api/users/login", { login, password });
   }
-  static async registration(email, password) {
-    return $api.post("/api/users/register", { email, password });
+  static async registration(login, email, password) {
+    return $api.post("/api/users/register", { login, email, password });
+  }
+  static async getProfile() {
+    return $api.get("/api/users/profile");
   }
   static async logout() {
     return $api.post("/api/users/logout");
   }
   static async checkAuth() {
     return $api.post("/api/users/refresh-token");
-  }
-  static async getProfile() {
-    return $api.get("/api/users/profile");
   }
 }
