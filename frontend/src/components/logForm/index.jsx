14b912ba9bafa3a23e3e96f027055e74070abb1a
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-17 13:30:43 +0300

    feat: add axios feat: configure inputs in login form feat: add login

--- frontend/src/components/logForm/index.jsx
+++ frontend/src/components/logForm/index.jsx
@@ -1,15 +1,30 @@
-import { useEffect } from "react";
+import UserService from "../../services/UserService";
 import styles from "./styles.module.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 export const LogForm = ({ setState, state }) => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
   const navigator = useNavigate();
   const auth = useSelector((state) => state.auth.isAuth);
   const dispatch = useDispatch();
   const setAuth = () => {
     dispatch({ type: "SET_AUTH", isAuth: true });
   };
- 
+  const logSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const responce = await UserService.login(login, password);
+      console.log(responce);
+      localStorage.setItem("token", responce.data.accessToken);
+      setInfo(responce.data.user);
+      setAuth(true);
+    } catch (e) {
+      console.log(e.responce?.data?.message);
+    }
+    navigator("/");
+  };
   return (
     <form className={styles.descriptionForm} action="">
       <h2 className={styles.descriptionFormH2}>Авторизация</h2>
@@ -20,34 +35,21 @@ export const LogForm = ({ setState, state }) => {
         placeholder={"Логин"}
         className={styles.descriptionFormInput}
         type="text"
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
       />
       <input
+        value={password}
         placeholder={"Пароль"}
         className={styles.descriptionFormInput}
         type="password"
+        onChange={(e) => setPassword(e.target.value)}
       />
       <div className={styles.descriptionFormButtons}>
         <button
           style={{ background: "#181F37" }}
           className={styles.descriptionFormButton}
-          onClick={(e) => {
-            e.preventDefault();
-            try {
-              const responce = await AuthService.login(email, password);
-              console.log(responce);
-              localStorage.setItem("token", responce.data.accessToken);
-              setAuth(true);
-              setInfo(responce.data.user);
-              setPassword("");
-            } catch (e) {
-              setError(true);
-              setPassword("");
-              console.log(e.responce?.data?.message);
-            }
-
-
-            navigator("/");
-          }}
+          onClick={logSubmit}
         >
           Продолжить
         </button>
