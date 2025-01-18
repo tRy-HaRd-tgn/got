466b8eeeb38d14253a95dc3894954b565e9229c6
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-18 17:56:47 +0300

    feat: add videos on main page

---
+++ frontend/src/pages/main/data/index.js
@@ -0,0 +1,10 @@
+export const data = [
+    {
+      url: "https://www.youtube.com/embed/S-bH1AcE0uc",
+      title:
+        "99% ИГРОКОВ СДАЛИСЬ БЫ НО НЕ СТАРЫЙ БОГ!😮🔥ОТ ЭТОГО ВИСПА ВРАЖЕСКАЯ КОМАНДА ДУРЕЕТ 13К ММР 7.37e",
+    },
+    { url: "https://www.youtube.com/embed/V3Jjc9DSGoU", title: "" },
+    { url: "https://www.youtube.com/embed/EPcz-Yk2Q20", title: "" },
+  ];
+  
