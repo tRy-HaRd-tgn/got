6e60de5346db90b92e24507b92cd83783dce3ed4
Даниил <mon.inch@yandex.ru>
2025-01-21 16:34:19 +0300

    Add validation for password 8 chars(1 number, 1 upper letter and 1 spec symbol)

--- backend/app/users/schemas.py
+++ backend/app/users/schemas.py
@@ -1,11 +1,29 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+import re
+from pydantic import BaseModel, EmailStr, validator
 
 
 class UserRegister(BaseModel):
     login: str
     email: EmailStr
     password: str
+
+    # Дополнительные проверки
+    @validator("login")
+    def validate_login(cls, v):
+        if len(v) < 3:
+            raise ValueError("Логин должен быть длиннее 3 символов")
+        return v
+
+    @validator("password")
+    def validate_password(cls, value):
+        # Регулярное выражение для пароля (минимум 8 символов, хотя бы одна цифра, одна заглавная буква, и один специальный символ)
+        password_regex = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+        if not re.match(password_regex, value):
+            raise ValueError(
+                "Пароль должен быть не менее 8 символов и содержать хотя бы одну цифру, одну заглавную букву и один специальный символ"
+            )
+        return value
 
 
 class UserLogin(BaseModel):
