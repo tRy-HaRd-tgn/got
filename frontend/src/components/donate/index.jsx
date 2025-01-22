414ee63d68e08b6cc84b840229d23db9cd652ab4
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-22 20:05:14 +0300

    feat: add backgrounds for the donate

--- frontend/src/components/donate/index.jsx
+++ frontend/src/components/donate/index.jsx
@@ -1,8 +1,15 @@
 import styles from "./styles.module.scss";
+import { useState } from "react";
+import { useEffect } from "react";
 export const DonateComp = ({ price, img, color, text }) => {
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    console.log(img);
+    setImage("http://localhost:8000"+img);
+  }, []);
   return (
     <div className={styles.wrapper}>
-      <img className={styles.wrapperImg} src={img} alt="error" />
+      <img className={styles.wrapperImg} src={image} alt="error" />
       <p className={styles.wrapperHeader}>{text}</p>
       <p className={styles.priceP}>
         {price}
