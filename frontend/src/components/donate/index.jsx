9366d6c1146a384d7d348ae51f72730fed7c3239
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-22 20:43:21 +0300

    feat: add color to the donate component

--- frontend/src/components/donate/index.jsx
+++ frontend/src/components/donate/index.jsx
@@ -2,12 +2,32 @@ import styles from "./styles.module.scss";
 import { useState } from "react";
 import { useEffect } from "react";
 export const DonateComp = ({ price, img, color, text }) => {
+  function hexToDec(hex) {
+    return parseInt(hex, 16);
+  }
+
+  const [rr, setRr] = useState(false);
+  const [gg, setGg] = useState(false);
+  const [bb, setBb] = useState(false);
   const [image, setImage] = useState(null);
   useEffect(() => {
     setImage("http://localhost:8000" + img);
+    setRr(hexToDec(color[1] + color[2]));
+    setGg(hexToDec(color[3] + color[4]));
+    setBb(hexToDec(color[5] + color[6]));
   }, []);
   return (
-    <div style={{ backgroundColor: color }} className={styles.wrapper}>
+    <div
+      style={{
+        background: `linear-gradient(
+      180deg,
+      rgba(${rr}, ${gg}, 73, 0.0625) 0%,
+      rgba(${rr}, ${gg}, ${bb}, 0.25) 100%
+    ),
+    linear-gradient(180deg, #111111 0%, #111111 50%, #1a1a1a 100%)`,
+      }}
+      className={styles.wrapper}
+    >
       <img className={styles.wrapperImg} src={image} alt="error" />
       <p className={styles.wrapperHeader}>{text}</p>
       <p className={styles.priceP}>
