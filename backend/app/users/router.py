77726e9337d4417db36ceb166e19a879b77bc40c
Даниил <mon.inch@yandex.ru>
2025-01-28 15:12:31 +0300

    Delete cache from profile

--- backend/app/users/router.py
+++ backend/app/users/router.py
@@ -69,7 +69,6 @@ async def register_user(user: UserRegister):
 
 
 @router.get("/profile", response_model=UserProfile)
-@cache(expire=120)
 async def get_profile(current_user: User = Depends(get_current_user)):
     """
     Возвращает информацию о текущем пользователе (личный кабинет).
