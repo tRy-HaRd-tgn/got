331a9e90ac89586c8435352f0eebbaa4991e17a3
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-01-29 21:08:58 +0300

    asd

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-//export const API_URL = "https://tortugagot.com/api";
-//export const API_URL2 = "https://tortugagot.com";
+export const API_URL = "https://tortugagot.com/api";
+export const API_URL2 = "https://tortugagot.com";
 
-export const API_URL = "http://localhost:8000/api";
-export const API_URL2 = "http://localhost:8000";
+//export const API_URL = "http://localhost:8000/api";
+//export const API_URL2 = "http://localhost:8000";
 
 
 
